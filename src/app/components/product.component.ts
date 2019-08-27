6227d15192952c2e8a26642deea2430528dc0084
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-08-27 17:29:25 -0500

    💄 work in styles

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -14,7 +14,8 @@ import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
-  templateUrl: './product.component.html'
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit, DoCheck, OnDestroy {
 
