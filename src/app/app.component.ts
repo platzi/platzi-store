60ab49daf2ab3f54deb25c87037928ee14391843
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-08-26 19:08:49 -0500

    🚧 ng for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  items = ['nicolas', 'julian', 'perez'];
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
