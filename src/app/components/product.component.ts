fd068b875805b72b751b678c500178b6ece2ad4c
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-08-27 16:56:56 -0500

    🚧 work

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  DoCheck,
+  OnDestroy
+} from '@angular/core';
 
 import { Product } from '../product.model';
 
@@ -6,10 +16,31 @@ import { Product } from '../product.model';
   selector: 'app-product',
   templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
 
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    console.log('1. constructor');
+  }
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //   console.log('2. ngOnChanges');
+  //   console.log(changes);
+  // }
+
+  ngOnInit() {
+    console.log('3. ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('4. ngDoCheck');
+  }
+
+  ngOnDestroy() {
+    console.log('5. ngOnDestroy');
+  }
 
   addCart() {
     console.log('añadir al carrito');
