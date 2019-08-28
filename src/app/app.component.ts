9680b043dca47eb1cde9927ec2e4e10f7c631875
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-08-28 14:57:50 -0500

    🚧 work in routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,76 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Product } from './product.model';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'platzi-store';
-
-  items = ['nicolas', 'julian', 'perez'];
-
-  objeto = {};
-
-  power = 10;
-
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '2',
-      image: 'assets/images/hoodie.png',
-      title: 'Hoodie',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '3',
-      image: 'assets/images/mug.png',
-      title: 'Mug',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '4',
-      image: 'assets/images/pin.png',
-      title: 'Pin',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '5',
-      image: 'assets/images/stickers1.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '6',
-      image: 'assets/images/stickers2.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-  ];
-
-  addItem() {
-    this.items.push('nuevo item');
-  }
-
-  deleteItem(index: number) {
-    this.items.splice(index, 1);
-  }
-
-  clickProduct(id: number) {
-    console.log('product');
-    console.log(id);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ProductComponent } from './components/product.component';
 import { CartComponent } from './cart/cart.component';
 import { ExponentialPipe } from './exponential.pipe';
 import { HighlightDirective } from './highlight.directive';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +20,10 @@ import { HighlightDirective } from './highlight.directive';
     CartComponent,
     ExponentialPipe,
     HighlightDirective,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    DemoComponent,
   ],
   imports: [
     BrowserModule,
