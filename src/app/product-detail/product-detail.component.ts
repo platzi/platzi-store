adc1077fdebe17dc66b470217f5bcbedb13b044a
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-08-28 16:42:44 -0500

    🚧 work
    work

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
 import { ProductsService } from './../products.service';
- 
+import { Product } from './../product.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,6 +10,8 @@ import { ProductsService } from './../products.service';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+
+  product: Product;
 
   constructor(
     private route: ActivatedRoute,
@@ -19,8 +21,7 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
-      const product = this.productsService.getProduct(id);
-      console.log(product);
+      this.product = this.productsService.getProduct(id);
     });
   }
 
