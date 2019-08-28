8daeb5207a1653bd6d51d5bb83e65fc55da74fa8
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-08-28 18:50:52 -0500

    🚧 work
    work

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { BannerComponent } from './components/banner/banner.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomeRoutingModule } from './home-routing.module';
   ],
   imports: [
     CommonModule,
+    SharedModule,
     HomeRoutingModule
   ]
 })
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
-import { ProductsService } from './../products.service';
+import { ProductsService } from './../core/services/products/products.service';
 import { Product } from './../product.model';
 
 @Component({
