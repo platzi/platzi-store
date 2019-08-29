938fbf8bda8d606eab565cdcda6e492517a4ee03
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-08-29 14:51:09 -0500

    🚧 angular material

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -7,6 +7,7 @@ import { ProductsComponent } from './components/products/products.component';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { SharedModule } from './../shared/shared.module';
+import { MaterialModule } from './../material/material.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { SharedModule } from './../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    MaterialModule
   ]
 })
 export class ProductModule {
