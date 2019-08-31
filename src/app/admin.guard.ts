6f80f109cfe78c01093822349a97b1882e2e497c
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-08-30 19:36:49 -0500

    🚧 work

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 
@@ -11,7 +11,8 @@ import { AuthService } from './core/services/auth.service';
 export class AdminGuard implements CanActivate {
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {
 
   }
@@ -20,8 +21,12 @@ export class AdminGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.hasUser().pipe(
-      tap(user => console.log(user)),
-      map(user => user === null ? false : true)
+      map(user => user === null ? false : true),
+      tap(hasUser => {
+        if (!hasUser) {
+          this.router.navigate(['/auth/login']);
+        }
+      }),
     );
   }
 
--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
+
+import { finalize } from 'rxjs/operators';
+
 import { MyValidators } from './../../../utils/validators';
 
 import { ProductsService } from './../../../core/services/products/products.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -13,11 +18,13 @@ import { ProductsService } from './../../../core/services/products/products.serv
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm();
   }
@@ -35,6 +42,25 @@ export class FormProductComponent implements OnInit {
         this.router.navigate(['./admin/products']);
       });
     }
+  }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const name = 'image.png';
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges()
+    .pipe(
+      finalize(() => {
+        this.image$ = fileRef.getDownloadURL();
+        this.image$.subscribe(url => {
+          console.log(url);
+          this.form.get('image').setValue(url);
+        });
+      })
+    )
+    .subscribe();
   }
 
   private buildForm() {
